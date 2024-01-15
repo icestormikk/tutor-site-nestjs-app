a9877d66a95b026a517f72e6e31b62e1288f6017
icestormikk <jigalow.pavel2015@yandex.ru>
2024-01-15 18:10:15 +0300

    adding documentation for the Prisma module

--- src/database/prisma.module.ts
+++ src/database/prisma.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+/**
+ * A module for connecting and interacting an application with a database
+ * @export
+ * @class PrismaModule
+ */
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -6,15 +6,31 @@ import {
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * A common service layer containing the main methods of interaction between the application and the database
+ * @export
+ * @class PrismaService
+ * @extends {PrismaClient}
+ * @implements {OnModuleInit}
+ * @implements {OnModuleDestroy}
+ */
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  /**
+   * program actions during module initialization
+   * @memberof PrismaService
+   */
   async onModuleInit() {
     await this.$connect();
   }
 
+  /**
+   * program actions when the module is turned off (for example: shutting down the server)
+   * @memberof PrismaService
+   */
   async onModuleDestroy() {
     await this.$disconnect();
   }
